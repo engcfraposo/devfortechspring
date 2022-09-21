b887873fe7a9eeee2754b6483bab036c61645d9f
Cláudio Rapôso <engcfraposo@gmail.com>
2022-09-21 16:51:18 -0300

    Create WebSecurityConfig.java

---
+++ src/main/java/com/devfortech/HelloWord/config/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.devfortech.HelloWord.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http
+			.httpBasic()
+			.and()
+			.authorizeHttpRequests()
+			.antMatchers("/h2-console/*", "/clients").permitAll()
+			.anyRequest().authenticated();
+
+		http.csrf().disable();
+
+		
+		return http.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService(AuthenticationManagerBuilder auth) throws Exception {	
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		
+		manager.createUser(User.withUsername("Ziriguidunho")
+				.password(passwordEncorder().encode("devfortech"))
+				.roles("ADMIN")
+				.build());
+		
+		return manager;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncorder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
